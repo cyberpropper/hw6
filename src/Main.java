96e6cac383d30cd43f3bc9b00ff666c2d3eaa71a
cyberpropper <osankin2002@mail.ru>
2024-05-29 00:27:45 +0700

    hw7

--- src/Main.java
+++ src/Main.java
@@ -29,22 +29,71 @@ public class Main {
         }
 
 
-        double totalMoney = 0;
-        double rate = 0.12;
-        for (int g = 1; g <= 12; g++){
-            double interest = totalMoney * rate/12;
-            totalMoney = totalMoney + 29000 + interest;
-            System.out.println("Месяц " + g + " сумма накоплений равна " + totalMoney + " рублей");
-        }
-
-
         int factor = 2;
         for (int i = 1; i <= 10; i++) {
             System.out.println(factor + "*" + i + "=" + (factor * i));
         }
 
+        // Начало дз №7
+
+        double totalMoney = 0;
+        double rate = 0.12;
+        int month = 0;
+        while (totalMoney <= 2459000){
+            double interest = totalMoney * rate/12;
+            totalMoney = totalMoney + 15000 + interest;
+            month = month + 1;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + totalMoney + " рублей");
+        }
+
+        byte a = 0;
+        while (a < 10) {
+            a++;
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (; a > 0; a--) {
+            System.out.print(a + " ");
+        }
+
+        int people = 12000000;
+        int diffPeople = people/1000;
+        int rait = 17;
+        int dead = 8;
+        for (int year = 1; year <= 10; year++) {
+            people = people + (diffPeople * (rait - dead));
+            System.out.println("Год " + year + ", численность начеления составляет " + people);
+        }
+
+        int savings = 15000;
+        int target = 9 * 12;
+        double interestRate = 0.07;
+        int months = 0;
+        while (months <= target) {
+            months++;
+            double monthlyInterest = savings * interestRate;
+            savings += monthlyInterest;
+            if (months % 6 == 0) {
+                System.out.println("Месяц " + months + ", сумма накоплений равна " + savings + " рублей");
+            }
+        }
+
+        int Friday = 5;
+        int dayWeek = Friday;
+        for (int dayMonth = Friday; dayMonth <= 31; dayMonth += 7) {
+            System.out.println("Сегодня пятница, " + dayMonth + "-е число. Необходимо подготовить отчет");
+            dayWeek += 7;
+        }
 
 
+        int cometLaunch = 0;
+        int startWeak = 1824;
+        int lastWeak = 2124;
+        for (; cometLaunch <= lastWeak; cometLaunch += 79) {
+            if (cometLaunch >= startWeak) {
+                System.out.println(cometLaunch);
+            }
+        }
 
     }
 }
